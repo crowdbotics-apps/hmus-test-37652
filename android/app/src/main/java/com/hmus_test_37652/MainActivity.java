ae2fb0cd41e908e5202df7d90175e4eb846ffc8b
Crowdbotics <team@crowdbotics.com>
2022-11-21 21:56:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/hmus_test_37652/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hmus_test_37652;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "hmus_test_37652";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hmus_test_37652",
+  url: "https://hmus_test_37652.botics.co",
+  api: "https://hmus_test_37652.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
